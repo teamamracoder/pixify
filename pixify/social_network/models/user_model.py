0b3f40416e2ec6e7268a34c6e18547a69d42ed4d
yoyobprotap@gmail.com <protapbarman02@gmail.com>
2024-11-11 00:57:26 +0530

    model update

--- pixify/social_network/models/user_model.py
+++ pixify/social_network/models/user_model.py
@@ -28,8 +28,8 @@ class User(models.Model):
     is_active = models.BooleanField(default= True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
-    # created_by = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True, default=None, related_name='fk_create_users_users_id')
-    # updated_by = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True, default=None, related_name='fk_update_users_users_id')
+    created_by = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True, default=None, related_name='fk_create_users_users_id')
+    updated_by = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True, default=None, related_name='fk_update_users_users_id')
     
     class Meta:
         db_table = 'users'
