a2e42f82c8e39cdba3034de22fc69c3269215ed6
Rahul-code-gif <ra1999sarkar@gmail.com>
2024-11-11 10:12:45 +0530

    helo

--- pixify/social_network/services/user_service.py
+++ pixify/social_network/services/user_service.py
@@ -4,16 +4,18 @@ from django.shortcuts import get_object_or_404
 def list_users():
     return models.User.objects.all()
 
-def create_user(first_name, last_name, email):
-    return models.User.objects.create(first_name=first_name, last_name=last_name, email=email)
+def create_user(first_name,middle_name, last_name, email,hobbies):
+    return models.User.objects.create(first_name=first_name,middle_name=middle_name, last_name=last_name, email=email,hobbies=hobbies)
 
 def get_user(user_id):
     return get_object_or_404(models.User, id=user_id)
 
-def update_user(user, first_name, last_name, email):
+def update_user(user, first_name,middle_name,last_name, email,hobbies):
     user.first_name = first_name
+    user.middle_name = middle_name
     user.last_name = last_name
     user.email = email
+    user.hobbies=hobbies
     user.save()
     return user
 
--- pixify/social_network/views/user_view.py
+++ pixify/social_network/views/user_view.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .. import services
+from ..constants import Gender, RelationShipStatus, Role
 
 class UserListView(View):
     def get(self, request):
+        
         users = services.user_service.list_users()
         return render(request, 'user/list.html', {'users': users})
 
 class UserCreateView(View):
     def get(self, request):
-        return render(request, 'user/create.html')
+        choices_gender = [{gender.value: gender.name} for gender in Gender]
+        choices_relationship_status = [{status.name: status.value} for status in RelationShipStatus]
+        return render(request, 'user/create.html',{"choices_gender":choices_gender,"choices_relationship_status":choices_relationship_status})
 
     def post(self, request):
         first_name = request.POST['first_name']
+        middle_name = request.POST['middle_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
-        services.user_service.create_user(first_name, last_name, email)
+        hobbies=request.POST['hobbies']
+        services.user_service.create_user(first_name,middle_name, last_name, email,hobbies)
         return redirect('user_list')
 
 class UserDetailView(View):
@@ -31,9 +37,11 @@ class UserUpdateView(View):
     def post(self, request, user_id):
         user = services.user_service.get_user(user_id)
         first_name = request.POST['first_name']
+        middle_name = request.POST['middle_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
-        services.user_service.update_user(user, first_name, last_name, email)
+        hobbies=request.POST['hobbies']
+        services.user_service.update_user(user, first_name,middle_name, last_name, email,hobbies)
         return redirect('user_detail', user_id=user.id)
 
 class UserDeleteView(View):
