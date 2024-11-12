9146c2c95f0324c15b09d064e9e4c8819832e895
sourik2004 <sourikgoswami123@gmail.com>
2024-11-13 00:39:35 +0530

    message_reaction

--- pixify/social_network/services/message_service.py
+++ pixify/social_network/services/message_service.py
@@ -1,4 +1,8 @@
 from ..models import Message
+from django.shortcuts import get_object_or_404
+
 
 def list_messages_by_chat_id(chat_id):
     return Message.objects.filter(chat_id=chat_id)
+def get_message(message_id):
+    return get_object_or_404(Message, id=message_id)
