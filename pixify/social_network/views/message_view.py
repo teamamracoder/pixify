1915583ef79c028d41a19a52c60b964b4632e366
partha <partha.sutradharmy@gmail.com>
2024-11-12 23:28:45 +0530

    view and service update and create

--- pixify/social_network/views/message_view.py
+++ pixify/social_network/views/message_view.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from ..services import message_service
 
 class messageView(View):
     def get(self, request):
+        return render(request, 'enduser/message/index.html')  
+
+class messageCreateView(View):
+    def get(self, request):        
+        text = "hi"
+        media_url = "null"
+        sender_id = 1 
+        chat_id = 2  
+
+        message_service.create_message(text, media_url, sender_id, chat_id)
+        return render(request, 'enduser/message/index.html')
+
+class messageUpdateView(View):
+    def get(self, request, message_id):
+        message = message_service.get_message(message_id)        
+        text = "hello"
+        media_url = "null"
+        message_service.update_message(message, text, media_url)
+        return render(request, 'enduser/message/index.html')
+
+class messageDeleteView(View):
+    def get(self, request, message_id):
+        message_service.delete_message(message_id)
         return render(request, 'enduser/message/index.html')
