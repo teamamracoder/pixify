62f9a979d03ea93401335bc0a0382d3cf2d73920
Rahul-code-gif <ra1999sarkar@gmail.com>
2024-11-14 00:53:36 +0530

    ami pari na ar

--- pixify/social_network/views/chat_view.py
+++ pixify/social_network/views/chat_view.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from .. import services
 
 # everyone insert data manually
 # user (5)
@@ -50,3 +51,56 @@ class ChatView(View):
         # mmebers 1,3
         # as memebers=multiple, then type=group
         # create new group 
+
+class ChatAdminListView(View):
+    def get(self, request):
+        chats = services.chat_service.list_chats()
+        return render(request, 'adminuser/chat/list.html',{'chats':chats})
+
+
+class ChatAdminCreateView(View):
+    def get(self, request):       
+        return render(request, 'adminuser/chat/create.html')
+
+    def post(self, request):
+        title=request.POST['title']
+        # member=request.POST['member']
+        type=request.POST['type']
+        is_active=request.POST.get('is_active' 'True')=='True'
+        created_at=request.POST.getlist('created_at')
+        updated_at=request.POST.getlist('updated_at')
+        created_by_id=request.POST.getlist('created_by_id')
+        updated_by_id=request.POST.getlist('updated_by_id')
+        chat_cover=request.POST.getlist('chat_cover')
+        services.chat_service.create_chats(title,type,is_active,created_at,updated_at,created_by_id,updated_by_id,chat_cover)
+        return redirect ('chat_list')
+    
+class ChatAdminDetailView(View):
+    def get(self, request, chat_id):
+        chat = services.chat_service.get_chat(chat_id)
+        return render(request, 'adminuser/chat/detail.html', {'chat': chat}) 
+        
+
+class ChatAdminUpdateView(View):
+    def get(self, request,chat_id):
+        chat = services.chat_service.get_chat(chat_id)
+        return render(request, 'adminuser/chat/update.html',{'chat': chat})
+
+    def post(self, request, chat_id):
+        chat = services.chat_service.get_chat(chat_id)
+        title = request.POST['title']
+        type = request.POST['type']
+        
+        services.chat_service.update_chats(chat, title,type)
+        return redirect('chat_detail', chat_id=chat.id)
+    
+
+class ChatAdminDeleteView(View):
+    def get(self, request, chat_id):
+        chat = services.chat_service.get_chat(chat_id)
+        return render(request, 'adminuser/chat/delete.html',{'chat': chat})
+
+    def post(self, request, chat_id):
+        chat= services.chat_service.get_chat(chat_id)
+        services.chat_service.delete_chats(chat)
+        return redirect('chat_list')
