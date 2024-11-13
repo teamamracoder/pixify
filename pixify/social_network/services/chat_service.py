ece7646af484f75327110d3773ccd4dbf8308340
partha <partha.sutradharmy@gmail.com>
2024-11-13 21:22:59 +0530

    view and service update and add

--- pixify/social_network/services/chat_service.py
+++ pixify/social_network/services/chat_service.py
@@ -2,7 +2,7 @@ from ..models import Chat, User, ChatMember
 from django.shortcuts import get_object_or_404
 
 def list_chats():
-    return models.Chat.objects.all()
+    return Chat.objects.all()
 
 def chat_details(chat_id):
     return get_object_or_404(Chat, id=chat_id)
@@ -44,20 +44,6 @@ def get_user_data(user_id):
         'followings': followings,
         'photo': photo,
     }
-
-
-
-# from .. import models
-# from django.shortcuts import get_object_or_404
-
-# def list_chat():
-#     return models.Chat.objects.all()
-
-# def create_chat(reciver,sender):
-#     return models.Chat.objects.create(reciver=reciver,sender=sender)
-
-# def get_chat_(chat_id):
-#     return get_object_or_404(models.Chat, id=chat_id)
 
 def details_chats(chat_id):
     return Chat.objects(chat_id)
