d0cd78233c3b676f1d4a4ede73bec460ff9253b6
partha <partha.sutradharmy@gmail.com>
2024-11-13 20:54:38 +0530

    view and service update and create

--- pixify/social_network/views/chat_view.py
+++ pixify/social_network/views/chat_view.py
@@ -1,5 +1,5 @@
 from django.views import View
-from ..services import chat_service, message_service
+from ..services import chat_service, message_service, user_service
 from django.shortcuts import render, redirect
 
 class ChatListView(View):
@@ -9,31 +9,30 @@ class ChatListView(View):
 
 class ChatCreateView(View):
     def get(self, request):    
+        # user = request.user        
+        user = user_service.get_user(2)
         title = "AbC"
-        members = "1,2,6"
-        chat_cover = "exampleurl.com"                        
-        chat_service.create_chat(title, members, chat_cover)        
-        return redirect('chat')
+        type = "2"
+        members = [1, 2, 3]  # Example member IDs
+        chat_cover = "exampleurl.com"
+        chat_service.create_chat(title, members, chat_cover,user,type)                            
+        return redirect('chat/')
 
 class ChatDetailsView(View):
     def get(self, request, chat_id):
         chat = chat_service.chat_details(chat_id)
         messages = message_service.list_messages_by_chat_id(chat_id)
         return render(request, 'enduser/chat/messages.html',{'chat':chat,'messages':messages})    
-class ChatUpdatesView(View):
-    def get(self,request, chat_id ):
-        # database data asbe
-        chat=chat_service.chat_details(chat_id)        
-        return 
     
-    def post(self, request, chat_id):
+class ChatUpdatesView(View):
+    def get(self, request, chat_id):
         # database data jabe
         chat  = chat_service.chat_details(chat_id)
-        titel = "AbC"
-        members = "1,2,3"
-        cover_photo = "exampleurl.com"
-        chat_service.update_chat(chat, titel, members, cover_photo)
-        return redirect('enduser/chat/chats.html') 
+        title = "XYZ"
+        members = [2, 4]
+        chat_cover = "example.com"
+        chat_service.update_chat(chat, title, members, chat_cover)        
+        return redirect('chat/')
     
 class ChatDeleteView(View):
     def get(self, request, chat_id):
