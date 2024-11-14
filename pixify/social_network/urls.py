29a7bb3a0d3db176471c963e65fe5c7fe8b9d8e3
Priya <priyamitra0606@gmail.com>
2024-11-14 11:03:04 +0530

    new update

--- pixify/social_network/urls.py
+++ pixify/social_network/urls.py
@@ -46,6 +46,8 @@ urlpatterns = [
 
     path('friendrequest/', views.FriendRequestView.as_view(), name='friendrequest'),
 
-    path('birthday/', views.BirthdayView.as_view(), name='birthday')
+    path('birthday/', views.BirthdayView.as_view(), name='birthday'),
+
+    path('friends/', views.FriendsView.as_view(), name='friends')
 
 ]
