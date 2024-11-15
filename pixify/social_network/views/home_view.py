703a4177d10eabffd8502c6a88092d3a79258843
Badhan Sarkar <badhansarkar164@gmail.com>
2024-11-15 22:01:59 +0530

    Update

--- pixify/social_network/views/home_view.py
+++ pixify/social_network/views/home_view.py
@@ -4,3 +4,11 @@ from django.views import View
 class HomeView(View):
     def get(self, request):
         return render(request, 'enduser/home/index.html')
+    
+
+# class HomeView(View):
+#     def get(self, request):
+#         return render(request, 'enduser/home/userprofile.html')
+
+    
+      
