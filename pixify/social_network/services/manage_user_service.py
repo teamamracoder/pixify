b131923f3c79d53791e793908cdf020cd93d26f2
yoyobprotap@gmail.com <protapbarman02@gmail.com>
2024-11-18 00:08:40 +0530

    team-c to temp-a and cleanup

--- pixify/social_network/services/manage_user_service.py
+++ pixify/social_network/services/manage_user_service.py
@@ -2,41 +2,6 @@ from ..models import User
 from django.shortcuts import get_object_or_404
 from django.db.models import Q   
 
-
-# def list_users():
-#     return User.objects.all()
-
-# def create_user(first_name, last_name, email):
-#     return User.objects.create(first_name=first_name, last_name=last_name, email=email)
-
-# def get_user(user_id):
-#     return get_object_or_404(User, id=user_id)
-
-# def update_user(user, first_name, last_name, email):
-#     user.first_name = first_name
-#     user.last_name = last_name
-#     user.email = email
-#     user.save()
-#     return user
-
-# def delete_user(user):
-#     user.delete()
-
-# def list_users_api(request):
-#     search_query = request.GET['search']
-#     if search_query:
-#         # recipient name/ group name should start with search param
-#         users = User.objects.filter(first_name__icontains=search_query).values()
-#     else:
-#         users = User.objects.all().values()
-#     return users
-
-# def get_user_by_email(email):
-#     return User.objects.filter(email=email).first()
-
-
-
-
 # admin-user section
 def manage_list_users(sort_by='first_name'):
     return User.objects.all().order_by(sort_by)
