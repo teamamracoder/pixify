1dad0dcfb17dbc28a57b9bb30bfa13cba02ebdaf
subham <modaksuham866@gmail.com>
2024-11-17 18:39:10 +0530

    add apis, ajax and some kind of canges into chatlistview

--- pixify/social_network/services/chat_service.py
+++ pixify/social_network/services/chat_service.py
@@ -1,8 +1,15 @@
-from ..models import Chat, User, ChatMember
+from ..models import Chat, User, ChatMember,Follower
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from django.db.models import Max
+
 
 def list_chats(user):
-    return models.Chat.objects.filter(members=user).exclude(members=user)
+    user_chats = Chat.objects.filter(members=user).annotate(
+        latest_message_timestamp=Max('fk_chat_messages_chats_id__send_at'),
+        latest_message=Max('fk_chat_messages_chats_id__text')  
+    ).order_by('-latest_message_timestamp')
+    return user_chats
 
 def chat_details(chat_id):
     return get_object_or_404(Chat, id=chat_id)
@@ -11,9 +18,9 @@ def create_chat(user, members, type, is_active):
             
     chat = Chat.objects.create(members=members, created_by=user,type=type, is_active=is_active)        
 
-    # for member_id in members:
-    #     member = User.objects.get(id=member_id)
-    #     ChatMember.objects.create(chat_id=chat, member_id=member, created_by=user)
+    for member_id in members:
+        member = User.objects.get(id=member_id)
+        ChatMember.objects.create(chat_id=chat, member_id=member, created_by=user)
     return chat
 
 def update_chat(chat, title, members, chat_cover):    
@@ -43,13 +50,35 @@ def get_user_data(user_id):
         'followings': followings,
         'photo': photo,
     }
-
 def details_chats(chat_id):
     return Chat.objects(chat_id)
-
 def get_chat (chat_id):
     return get_object_or_404(Chat, id=chat_id)
+def user_follow(user):
+    followers = Follower.objects.filter(follower=user, is_active=True).select_related('following')
+    followings = Follower.objects.filter(following=user, is_active=True).select_related('follower')
+    return followers, followings
 
+def list_chats_api(request,user):
+    search_query = request.GET.get('search',)
+    if search_query:
+        chats = Chat.objects.filter(members=user,title__icontains=search_query).values()
+    else:
+        chats =  Chat.objects.filter(members=user).values()
+    return chats
+    
+def list_followers_api(request,user):
+    search_query = request.GET.get('search','')
+    if search_query:
+        followers = Follower.objects.filter(follower=user,fk_follower_users__icontains=search_query).values()
+        followings = Follower.objects.filter(following=user,fk_following_users__icontains=search_query).values()
+    else:
+       followers = User.objects.all().values()
+    return followers,followings
+def chat_member(chat_id,user):
+    chat_member = ChatMember.objects.exclude(member_id=user.id).get(chat_id=chat_id)
+    member = chat_member.member_id 
+    return member
 
 
 # chat service
--- pixify/social_network/services/message_service.py
+++ pixify/social_network/services/message_service.py
@@ -1,8 +1,13 @@
-from ..models import Message
+from ..models import Message,MessageReadStatus
 from django.shortcuts import get_object_or_404
 
-def list_messages_by_chat_id(chat_id):
-    return Message.objects.filter(chat_id=chat_id)
+def list_messages_by_chat_id(chat_id, unread_messages,user):
+            for message in unread_messages:
+                MessageReadStatus.objects.create(
+                message_id=message,
+                read_by=user
+            )
+            return Message.objects.filter(chat_id=chat_id,is_active=True)
 
 
 def create_message(text, media_url, sender_id, chat_id):
@@ -26,3 +31,11 @@ def delete_message(message_id):
         message = get_object_or_404(message, id=message_id)
         message.is_active=False
         message.save()
+
+def unread_count(chat,user):    
+    unread_count = Message.objects.filter(
+        chat_id=chat
+        ).exclude(
+        fk_message_msg_status_messages_id__read_by=user
+    ).count()  
+    return unread_count
