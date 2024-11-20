f6d75c673682351d92cac7b3ed28cceeeb0d46cc
diptibiswas <diptibca123@gmail.com>
2024-11-20 21:59:05 +0530

    Use Django Forms

--- pixify/social_network/views/manage_notification_view.py
+++ pixify/social_network/views/manage_notification_view.py
@@ -1,19 +1,14 @@
-
 from django.http import HttpResponseBadRequest, JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import View 
-
 from .. import services
-from ..models import User
-from django.core.paginator import Paginator 
-
-from social_network.constants.default_values import Role
-from social_network.decorators.exception_decorators import catch_error
-
+from ..models import Notification
+from django.core.paginator import Paginator
+from ..constants.default_values import Role
+from ..decorators.exception_decorators import catch_error
 from ..decorators import auth_required, role_required
-from social_network.packages.response import success_response
-
-
+from ..packages.response import success_response
+from ..forms.manage_notification_forms import ManageNotificationCreateForm, ManageNotificationUpdateForm
     
 class ManageNotificationListView(View):
 
@@ -40,72 +35,75 @@ class ManageNotificationListView(View):
         page_obj = paginator.get_page(page_number)
 
         return render(request, 'adminuser/notification/list.html', {
-            'notifications': page_obj,
-            # 'choices_gender': choices_gender,
+            'notifications': page_obj,           
             'sort_by': sort_by,
             'sort_order': sort_order,
             'search_query': search_query,  # Ensure this is being passed to the template
             'page_obj': page_obj,
         })
-
-class ManageNotificationCreateView(View):
-    def get(self, request):
-        return render(request, 'adminuser/notification/create.html')
-  
-    def post(self, request):
-        notification_data = {
-            'receiver_id': User.objects.get(id=request.POST['receiver_id']),
-            'created_by': User.objects.get(id=request.POST['created_by']),
-            'text': request.POST['text'],           
-            'media_url': request.POST.get('media_url', ''), 
-            'is_read': request.POST['is_read']            
-
-        }
-        services.manage_notification_service.manage_create_notification(**notification_data)      
-        return redirect('manage_notification_list')         
+     
 
 class ManageNotificationDetailView(View):
     def get(self, request, notification_id):
         notification = services.manage_notification_service.manage_get_notification(notification_id)
         return render(request, 'adminuser/notification/detail.html',{'notification': notification})
 
+
+class ManageNotificationCreateView(View):
+    @catch_error
+    def get(self, request):
+        form = ManageNotificationCreateForm()
+        return render(request, 'adminuser/notification/create.html', {"form": form})
+
+    # @catch_error
+    def post(self, request):
+        user=request.user
+        # if request.method == 'POST':
+        form = ManageNotificationCreateForm(request.POST)
+        if form.is_valid():
+            notification_data = {
+                'text': form.cleaned_data['text'],
+                'media_url': form.cleaned_data['media_url'],
+                'is_read': form.cleaned_data['is_read'],
+                'receiver_id':user,
+                # 'created_by': form.cleaned_data['created_by'], 
+                # 'created_by': User.objects.get(id=request.POST['created_by']),
+                'created_by': user
+            }
+            services.manage_notification_service.manage_create_notification(**notification_data)
+            return redirect('manage_notification_list')
+        return render(request, 'adminuser/notification/create.html', {"form": form})
+
+
 class ManageNotificationUpdateView(View):
+    @catch_error
+    def get(self, request, notification_id):
+        notification = get_object_or_404(Notification, id=notification_id)  # Assuming you have a User model
+        form = ManageNotificationUpdateForm(initial={
+            'text': notification.text,
+            'media_url': notification.media_url,
+            'receiver_id': notification.receiver_id.id,
+            'is_raed': notification.is_read          
+        })
+        return render(request, 'adminuser/notification/update.html', {"form": form, "notification_id": notification.id})
 
     @catch_error
-    @auth_required
-    @role_required(Role.ADMIN.value, Role.END_USER.value)  
-
-    def get(self, request, notification_id):       
-        notification = services.manage_notification_service.manage_get_notification(notification_id)
-        return render(request, 'adminuser/notification/update.html', {'notification': notification})
-    
     def post(self, request, notification_id):
-        notification = services.manage_notification_service.manage_get_notification(notification_id)
+        notification = get_object_or_404(Notification, id=notification_id)
+        form = ManageNotificationUpdateForm(request.POST)
+        if form.is_valid():
+            notification.text = form.cleaned_data['text']
+            notification.media_url = form.cleaned_data.get('media_url')
 
-        # Gather all form data into a dictionary
-        notification_data = {
-            'text': request.POST.get('text'),
-            'media_url': request.POST.get('media_url'),
-            'receiver_id': User.objects.get(id=request.POST['receiver_id']),
-            'is_read': request.POST.get('is_read')           
-        }
-
-        # Validate required fields
-        required_fields = ['text', 'media_url', 'receiver_id', 'is_read']
-        for field in required_fields:
-            if not notification_data.get(field):
-                return HttpResponseBadRequest(f"Missing required field: {field}")
-
-        # Update the user with the provided data
-        services.manage_notification_service.manage_update_notification(
-            notification, 
-            **notification_data  # Pass the dictionary as keyword arguments
-        )
-        return redirect('manage_notification_detail', notification_id=notification.id)
+            # notification.receiver_id = form.cleaned_data['receiver_id']
+            # notification.is_raed = form.cleaned_data['is_raed']
+            
+            notification.save()  # Save the updated user instance
+            return redirect('manage_notification_list')
+        return render(request, 'adminuser/notification/update.html', {"form": form, "notification_id": notification.id})
 
 
 class ManageToggleNotificationActiveView(View):
-
      def post(self, request, notification_id):
         notification = services.manage_notification_service.manage_get_notification(notification_id)
         notification.is_active = not notification.is_active  
