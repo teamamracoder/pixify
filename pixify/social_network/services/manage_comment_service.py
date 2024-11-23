ec5e7c6365c1248b7b9528eb5af0740fdb1273e1
Priya <priyamitra0606@gmail.com>
2024-11-23 11:36:26 +0530

    added

--- pixify/social_network/services/manage_comment_service.py
+++ pixify/social_network/services/manage_comment_service.py
@@ -1,16 +1,3 @@
-# from .. import models
-# from django.shortcuts import get_object_or_404
-# from django.db.models import Q 
-
-
-# def manage_create_comment(**kwargs):
-    
-#     comment = models.Comment.objects.create(
-#             comment=kwargs['comment'] ,
-#             post_id=kwargs['post_id']
-
-#         )
-#     return comment
 from tokenize import Comment
 from ..packages.get_data import GetData
 from .. import models
@@ -20,10 +7,13 @@ from django.shortcuts import get_object_or_404
 def manage_create_comment(**kwargs):
     comment = models.Comment.objects.create(      
             comment=kwargs['comment'],           
-            post_id=kwargs['post_id']
-                        
+            post_id=kwargs['post_id'],
+            comment_by=kwargs['comment_by'],
+            created_by=kwargs['created_by']             
         )
+    
     return comment
+
 
 
 
@@ -39,4 +29,5 @@ def manage_list_comments_filtered(search_query, sorting_order, sort_by, page_num
         .paginate(limit=3, page=page_number)
         .execute()
     )
+
     return data
--- pixify/social_network/views/manage_comment_view.py
+++ pixify/social_network/views/manage_comment_view.py
@@ -1,47 +1,12 @@
-# from django.http import HttpResponseBadRequest, JsonResponse
-# from django.shortcuts import render, redirect
-# from django.views import View 
-# from .. import services
-# from ..models import User
-# from django.core.paginator import Paginator 
-# from social_network.constants.default_values import Role
-# from social_network.decorators.exception_decorators import catch_error
-# from ..decorators import auth_required, role_required
-# from social_network.packages.response import success_response
-# from ..forms.manage_comment_form import ManageCommentCreateForm
-
-
-
-
-# class CommentListView(View):
-#      def get(self, request):
-#         return render(request, 'adminuser/comment/list.html')
-     
-
-# class CommentCreateView(View):
-
-#    def post(self,request):
-#          form=ManageCommentCreateForm()
-#          return render(request, 'adminuser/comment/create.html', {"form": form})
-   
-   
-#    def post(self, request):
-     
-#         form = ManageCommentCreateForm(request.POST)
-#         comment_data = {
-#             'comment': form.cleaned_data['comment'],
-#             'post_id': form.cleaned_data['post_id']
-#                 }
-#         services.manage_comment_service.manage_create_comment(**comment_data)
-#         return redirect('manage_comment_list')
-       
-
-
 
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import get_object_or_404, render, redirect
-from django.views import View 
-from .. import services
+from django.views import View
+
+from ..models.post_model import Post
+
+
+from ..import services
 from ..models import Notification
 from django.core.paginator import Paginator
 from ..constants.default_values import SortingOrder
@@ -50,24 +15,58 @@ from ..decorators import auth_required, role_required
 from ..packages.response import success_response
 from ..forms.manage_comment_form import ManageCommentCreateForm
     
+
+# class ManageCommentListView(View):
+#     @catch_error
+#     def get(self, request):
+#         # Fetch the search query from the URL parameters
+#         # search_query = request.GET.get('search', '')
+#         # sort_by = request.GET.get('sort_by', "created_at")
+#         # sort_order = request.GET.get('sort_order', SortingOrder.DESC.value)
+#         # page_number = request.GET.get('page', 1)
+
+#         # get data
+#         data= services.manage_comment_service.manage_list_comments_filtered(
+#             # search_query=search_query,
+#             # sort_by=sort_by,
+#             # sorting_order=sort_order,
+#             # page_number=page_number
+#         )
+#         print("this is comment pagedata",data)
+       
+#         return render(request,'adminuser/comment/list.html',
+#             success_response("User data fetched successfully", data)
+#         ) 
+       
+
+ 
+
 class ManageCommentListView(View):
     @catch_error
     def get(self, request):
         # Fetch the search query from the URL parameters
-      #   search_query = request.GET.get('search', '')
-      #   sort_by = request.GET.get('sort_by', "created_at")
-      #   sort_order = request.GET.get('sort_order', SortingOrder.DESC.value)
-      #   page_number = request.GET.get('page', 1)
+        search_query = request.GET.get('search', '')
+        sort_by = request.GET.get('sort_by', "created_at")
+        sort_order = request.GET.get('sort_order', SortingOrder.DESC.value)
+        page_number = request.GET.get('page', 1)
 
         # get data
-      #   data = services.manage_comment_service.manage_list_comments_filtered(
-      #       search_query=search_query,
-      #       sort_by=sort_by,
-      #       sorting_order=sort_order,
-      #       page_number=page_number
-      #   )
-        return render(request,'adminuser/comment/list.html',success_response("Comment data fetched successfully")
+        data = services.manage_comment_service.manage_list_comments_filtered(
+            search_query=search_query,
+            sort_by=sort_by,
+            sorting_order=sort_order,
+            page_number=page_number
+        )
+        # return
+        return render(
+            request,
+            'adminuser/comment/list.html',
+            success_response("User data fetched successfully", data)
         ) 
+    
+
+
+
 
 
 class ManageCommentCreateView(View):
@@ -78,16 +77,22 @@ class ManageCommentCreateView(View):
 
     # @catch_error
     def post(self, request):
+        user=request.user
         form = ManageCommentCreateForm(request.POST)
         if form.is_valid():
             comment_data = {
                 'comment': form.cleaned_data['comment'],
-                'post_id': form.cleaned_data['post_id']
+                'post_id': Post.objects.get(id=request.POST['post_id']),
+                'comment_by':user,
+                'created_by':user
+                
                
             }
             services.manage_comment_service.manage_create_comment(**comment_data)
             return redirect('manage_comment_list')
         return render(request, 'adminuser/comment/create.html', {"form": form})
+
+
 
 class ManageToggleCommentActiveView(View):
      def post(self, request, comment_id):
