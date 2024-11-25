8924dacb7e54a17c23e81f7999745254dd8ae5a3
pythonprojectofficial <pythonproject066@gmail.com>
2024-11-25 14:20:29 +0530

    no change

--- pixify/pixify/asgi.py
+++ pixify/pixify/asgi.py
@@ -10,18 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from social_network import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pixify.settings')
 
 application = get_asgi_application()
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             routing.websocket_urlpatterns
-#         )
-#     ),
-# })
