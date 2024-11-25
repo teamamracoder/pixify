d6212cfe48d647fab7b3e709c64eb53adcfd8c9d
pythonprojectofficial <pythonproject066@gmail.com>
2024-11-25 14:15:49 +0530

    remove some installed apps (releted to socket.io) due to no need this time

--- pixify/pixify/settings.py
+++ pixify/pixify/settings.py
@@ -18,9 +18,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'channels',
-    'socketio',
     'social_network'
 ]
 
