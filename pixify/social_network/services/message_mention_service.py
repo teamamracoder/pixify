917aa8261f7641e9ae027837fff3ad5f98cb4fd0
partha <partha.sutradharmy@gmail.com>
2024-11-27 22:22:01 +0530

    Message mentions

--- pixify/social_network/services/message_mention_service.py
+++ pixify/social_network/services/message_mention_service.py
@@ -18,11 +18,13 @@ def list_messages_mention_Api(chat, user, search_query, exclude_ids, mentioned_a
         ).values(
             'member_id', 'member_id__first_name', 'member_id__last_name', 'member_id__profile_photo_url'
         )
+        print(f"Filtered mentions with search query '{search_query}': {mention}")
     else:
         mention = ChatMember.objects.filter(chat_id=chat).exclude(member_id=user).exclude(member_id__in=exclude_ids).values(
             'member_id', 'member_id__first_name', 'member_id__last_name', 'member_id__profile_photo_url'
         )
-        
+        print("Mentions without search query:", mention)
+
     mention_list = list(mention)
     if not mentioned_all:
         mention_list.insert(0, {'member_id': 'all', 'member_id__first_name': 'All', 'member_id__last_name': '', 'member_id__profile_photo_url': 'all-photo'})
