aa8fab337e8b782a9d0aeac6a287aa6bc5bc3954
rimamdt <rimadasmdt@gmail.com>
2024-11-29 02:01:46 +0530

    hdfjhd

--- pixify/social_network/views/notification_view.py
+++ pixify/social_network/views/notification_view.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render, redirect
-from django.views import View 
+from django.views import View
+from .. import services
+
+ 
+
 
 class NotificationView(View):
     def get(self, request):
-        return render(request, 'enduser/notification/index.html')
+       notifications_fetch=services.user_Notification_service.count_notification()
+       notification_list=list(notifications_fetch.values())
+    #    notify={
+    #          'notifications_fetch':notifications_fetch,
+    #       }
+       print(notification_list)
+       return render(request, 'enduser/notification/index.html',{'notification_list':notification_list})
     
-
+    
