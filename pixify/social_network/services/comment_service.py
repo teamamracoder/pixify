ee72e01d7123e9b13ba7565ddef20b0be6bcfbd1
Priya <priyamitra0606@gmail.com>
2024-11-28 19:11:03 +0530

    afa

--- pixify/social_network/services/comment_service.py
+++ pixify/social_network/services/comment_service.py
@@ -8,8 +8,8 @@ from .. import models
 
 
 
-def user_comments_create(commentstext,user_id):
-    return Comment.objects.create(comment=commentstext,created_by_id=user_id,comment_by_id=user_id)
+def user_comments_create(commentstext,user_id,post_id):
+    return Comment.objects.create(comment=commentstext,created_by_id=user_id,comment_by_id=user_id,post_id_id=post_id)
 
 
 def get_count_comment(post_id):
