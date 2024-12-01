b0f74c67fd1130383e605eb04da15388d9eae092
Rahul-code-gif <ra1999sarkar@gmail.com>
2024-12-01 20:42:22 +0530

    coment

--- pixify/social_network/services/manage_chat_service.py
+++ pixify/social_network/services/manage_chat_service.py
@@ -11,14 +11,17 @@ def manage_list_chats(sort_by='title'):
 def manage_create_chats(title,type,created_by,chat_cover):
   return Chat.objects.create(title=title,type=type,created_by=created_by,chat_cover=chat_cover)
 
-def manage_get_member(chat_id):   
-    chat_member = ChatMember.objects.filter(chat_id=chat_id )
+def manage_get_member(chat_id): 
+    chat_members = ChatMember.objects.filter(chat_id=chat_id )
+    member_id = [member.member_id for member in chat_members]
+    chat_member = User.objects.filter(id=1)
     return chat_member
 
-def get_chat_member_by_member_id(chat_member):
-#    user_chat_member= User.objects.filter(id=chat_member, is_active=True)
-   user_chat_member= User.objects.all()
-   return user_chat_member
+#  def get_chat_member_by_member_id(chat_id):
+#     chat_members = ChatMember.objects.filter(chat_id=chat_id)
+#     user_chat_members = [member.user for member in chat_members]
+#     return user_chat_members
+
 
 def manage_get_chat(chat_id):
    return get_object_or_404 (Chat , id=chat_id)
