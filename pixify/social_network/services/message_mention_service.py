b162a474ed5db1a9a33dcccc342de56865d2564d
partha <partha.sutradharmy@gmail.com>
2024-12-01 14:08:10 +0530

    Right the logic of Message Mentions

--- pixify/social_network/services/message_mention_service.py
+++ pixify/social_network/services/message_mention_service.py
@@ -13,36 +13,52 @@ def delete_message_mentions(message,user):
         mention.save()
 
 
+
+
+
 def list_messages_mention_Api(chat, user, search_query, exclude_ids, mentioned_all):
     print_log(f"Search Query: {search_query}, Exclude IDs: {exclude_ids}, Mentioned All: {mentioned_all}")
-    
+
+    # If @all is mentioned, return an empty list to avoid showing any suggestions
+    if mentioned_all:
+        return []
+
     numeric_ids = [id for id in exclude_ids if id.isdigit()]
     string_mentions = [id for id in exclude_ids if not id.isdigit()]
-        
+
     user_ids_to_exclude = numeric_ids[:]
 
     for username in string_mentions:
         if username != 'all': 
-            # mentioned_all=not(mentioned_all)
-    
             user_obj = User.objects.filter(first_name=username).first()
             if user_obj:
                 user_ids_to_exclude.append(str(user_obj.id))
-    print_log(mentioned_all)
+
     print_log(f"Resolved Exclusion IDs: {user_ids_to_exclude}")
-    
+
+    # Get the list of members in the chat, excluding the current user
     mention = ChatMember.objects.filter(chat_id=chat).exclude(member_id=user)
-    
+
     if user_ids_to_exclude:
         mention = mention.exclude(member_id__in=user_ids_to_exclude)
-    
+
     if search_query:
-        mention = mention.filter(Q(member_id__first_name__icontains=search_query) |Q(member_id__last_name__icontains=search_query))
-    
-    mention_list = list(mention.values('member_id','member_id__first_name','member_id__last_name','member_id__profile_photo_url'))
-    
+        mention = mention.filter(
+            Q(member_id__first_name__icontains=search_query) |
+            Q(member_id__last_name__icontains=search_query)
+        )
+
+    mention_list = list(mention.values('member_id', 'member_id__first_name', 'member_id__last_name', 'member_id__profile_photo_url'))
+
+    # Always include @all option if mentioned_all is False
     if not mentioned_all:
-        mention_list.insert(0, {"member_id": "all","member_id__first_name": "All","member_id__last_name": "","member_id__profile_photo_url": "/path/to/all/icon.png"})
+        mention_list.insert(0, {
+            "member_id": "all",
+            "member_id__first_name": "All",
+            "member_id__last_name": "",
+            "member_id__profile_photo_url": '<i class="fas fa-users" style="color:black; font-size:18px;"></i>',
+        })
 
     print_log(f"Mention List: {mention_list}")
     return mention_list
+
