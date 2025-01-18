059b7328430030cdb148f6eebcb3e1f7f37c1607
partha <partha.sutradharmy@gmail.com>
2025-01-18 22:58:26 +0530

    problem solve

--- pixify/social_network/views/chat_view.py
+++ pixify/social_network/views/chat_view.py
@@ -148,6 +148,7 @@ class ChatCreateView(View):
                 )                
 
             chat = chat_service.create_chat(user, None, None, ChatType.PERSONAL.value)            
+            chat_member_service.add_chat_member(chat.id, user.id, user)
             chat_member_service.add_chat_member(chat.id, member, user)
 
             return JsonResponse({
