c6a89b3f551c8dc61c217a81b7a40acac4a3a129
shubhankarchdas <shubhankarchdas@gmail.com>
2025-01-22 18:09:54 +0530

    calling_feature implementation

--- pixify/social_network/routing.py
+++ pixify/social_network/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<chat_id>\d+)/$', consumers.ChatConsumer.as_asgi()),  # Capture chat_id dynamically
+    re_path(r'ws/call/(?P<call_id>[a-zA-Z0-9]+)/$', consumers.CallConsumer.as_asgi()),
 ]
--- pixify/social_network/urls.py
+++ pixify/social_network/urls.py
@@ -165,6 +165,8 @@ urlpatterns = [
     # path('', views.UserStoryListView.as_view(), name='Userstory_list'),
     path('uploadStory/', views.UploadStoryView.as_view(),name='uploadStory'),
 
+    path('call/<str:call_id>', views.CallView.as_view(),name='call_page'),
+
 ]
 
 
