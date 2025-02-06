4d7cfa22f0e10893867846c3486d4e3f6059f552
Priya <priyamitra0606@gmail.com>
2025-02-06 19:17:38 +0530

    a

--- pixify/social_network/services/post_reaction_service.py
+++ pixify/social_network/services/post_reaction_service.py
@@ -1,8 +1,8 @@
 from ..models import PostReaction,Post,User
 
 
-def post_reactionby_name(reaction_id):
-    return PostReaction.objects.filter(id=reaction_id).first()
+def post_reactionby_name(post_id):
+    return PostReaction.objects.filter(post_id_id=post_id).first()
 
 def create_post_reaction(post_id,user_id):
     created = PostReaction.objects.create(post_id_id=post_id,reacted_by_id=user_id,
@@ -15,14 +15,6 @@ def create_or_update_message_reaction(post_id,user_id):
                                           created_by_id= user_id,is_active= True)
    return created
 
-# def get_reaction_count(post_id, reaction_id):
-
-#     return PostReaction.objects.filter(
-#         post_id=post_id,
-#         reaction_id=reaction_id,
-#         is_active=True
-#     ).count()
-#     return
 
 
 def get_reaction_count(post_id, reaction_id):
