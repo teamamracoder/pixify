d4243c20c9854bfff6dbe08d00a48fbdce55306f
shubhankarchdas <shubhankarchdas@gmail.com>
2025-02-09 21:12:55 +0530

    call_feature implementation

--- pixify/social_network/views/call_view.py
+++ pixify/social_network/views/call_view.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from ..decorators.exception_decorators import catch_error
+from social_network.constants.default_values import Role
+from ..decorators import auth_required, role_required
 
 class CallView(View):
-    def get(self,request):
-        context={}
-        return render(request,'enduser/chat/call.html',context=context)
+    @catch_error
+    @auth_required
+    @role_required(Role.ADMIN.value, Role.END_USER.value)
+    def get(self, request, call_id):
+        template_name = 'enduser/chat/calling_feature.html'
+        context = {
+            'call_id': call_id,  # Pass the call_id to the template
+        }
+        return render(request, template_name, context)
