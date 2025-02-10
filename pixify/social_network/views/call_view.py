03e5ae52994c2072bdb54017b8c10b7440a39332
partha <partha.sutradharmy@gmail.com>
2025-02-10 21:40:54 +0530

    Fix conflict

--- pixify/social_network/views/call_view.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-class CallView(View):
-    def get(self, request, call_id): 
-        context = {'call_id': call_id}  
-        return render(request, 'enduser/chat/ringing.html', context=context)
