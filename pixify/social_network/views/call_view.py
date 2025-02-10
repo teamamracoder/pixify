ec25e39349420bdb2a7fd62b61ef10d68e207759
partha <partha.sutradharmy@gmail.com>
2025-02-10 21:27:39 +0530

    Fix conflict

--- pixify/social_network/views/call_view.py
+++ pixify/social_network/views/call_view.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-from ..decorators.exception_decorators import catch_error
-from social_network.constants.default_values import Role
-from ..decorators import auth_required, role_required
 
 class CallView(View):
-    @catch_error
-    @auth_required
-    @role_required(Role.ADMIN.value, Role.END_USER.value)
-    def get(self, request, call_id):
-        template_name = 'enduser/chat/calling_feature.html'
-        context = {
-            'call_id': call_id,  # Pass the call_id to the template
-        }
-        return render(request, template_name, context)
+    def get(self, request, call_id): 
+        context = {'call_id': call_id}  
+        return render(request, 'enduser/chat/ringing.html', context=context)
