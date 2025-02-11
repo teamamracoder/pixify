58b53fb438b243a27804edbe1b6fd701070bfb62
subham <modaksuham866@gmail.com>
2025-02-11 15:27:02 +0530

    "voice calling feature is now connected successfully"

--- pixify/social_network/consumers.py
+++ pixify/social_network/consumers.py
@@ -426,17 +426,48 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class CallConsumer(AsyncWebsocketConsumer):
+    active_users = {}  # Dictionary to track active users in each call
+
     async def connect(self):
         self.chat_id = self.scope['url_route']['kwargs']['chat_id']
         self.call_group_name = f"call_{self.chat_id}"
+
+        # Add user to active users list
+        if self.call_group_name not in self.active_users:
+            self.active_users[self.call_group_name] = 0
+        self.active_users[self.call_group_name] += 1
 
         # Join room group
         await self.channel_layer.group_add(self.call_group_name, self.channel_name)
         await self.accept()
 
+        # Notify others about user join
+        await self.channel_layer.group_send(
+            self.call_group_name,
+            {
+                "type": "user.joined",
+                "active_users": self.active_users[self.call_group_name]
+            }
+        )
+
     async def disconnect(self, close_code):
+        # Remove user from active users list
+        if self.call_group_name in self.active_users:
+            self.active_users[self.call_group_name] -= 1
+            if self.active_users[self.call_group_name] <= 0:
+                del self.active_users[self.call_group_name]
+
         # Leave room group
         await self.channel_layer.group_discard(self.call_group_name, self.channel_name)
+
+        # Notify others about user leave
+        await self.channel_layer.group_send(
+            self.call_group_name,
+            {
+                "type": "user.left",
+                "active_users": self.active_users.get(self.call_group_name, 0)
+            }
+        )
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -470,6 +501,32 @@ class CallConsumer(AsyncWebsocketConsumer):
                 }
             )
 
+        elif action == "user_joined":
+            await self.channel_layer.group_send(
+                self.call_group_name,
+                {
+                    "type": "user.joined",
+                    "active_users": self.active_users[self.call_group_name]
+                }
+            )
+
+        elif action == "user_left":
+            await self.channel_layer.group_send(
+                self.call_group_name,
+                {
+                    "type": "user.left",
+                    "active_users": self.active_users.get(self.call_group_name, 0)
+                }
+            )
+        elif action == "call_accepted":
+            await self.channel_layer.group_send(
+                self.call_group_name,
+                {
+                    "type": "call.accepted",
+                    "call_id": data["call_id"]
+                }
+            )
+
     async def call_started(self, event):
         await self.send(text_data=json.dumps({"action": "call_started", "call_id": event["call_id"]}))
 
@@ -478,3 +535,10 @@ class CallConsumer(AsyncWebsocketConsumer):
 
     async def webrtc_signal(self, event):
         await self.send(text_data=json.dumps({"action": "webrtc_signal", "signal": event["signal"], "from": event["from"]}))
+
+    async def user_joined(self, event):
+        await self.send(text_data=json.dumps({"action": "user_joined", "active_users": event["active_users"]}))
+
+    async def user_left(self, event):
+        await self.send(text_data=json.dumps({"action": "user_left", "active_users": event["active_users"]}))
+
