d7a217c5270d1e59687f605424b4e29b49d6aa5c
Badhan Sarkar <badhansarkar164@gmail.com>
2025-02-12 20:50:13 +0530

    Fix the short

--- pixify/social_network/views/short_view.py
+++ pixify/social_network/views/short_view.py
@@ -15,7 +15,6 @@ class ShortListView(View):
             comments = short_service.comment_count(short.id)
             short.comments_count = short_service.format_count(comments)
             short.user_reacted = short_service.user_has_reacted(short, user)
-
         random.shuffle(shorts)  # Randomize the list
         return render(request, 'enduser/short/index.html', {'shorts': shorts})          
 
