4de2605ed2cf6c7908100273c902449a4c8ff4ae
partha <partha.sutradharmy@gmail.com>
2025-02-12 13:42:05 +0530

    Nothing Change

--- pixify/social_network/services/message_read_status_service.py
+++ pixify/social_network/services/message_read_status_service.py
@@ -1,6 +1,6 @@
 from ..models import MessageReadStatus
 
-def create_message_read_status(message_id,user):
+def create_message_read_status(message_id, user):
     MessageReadStatus.objects.create(
         message_id=message_id,
         read_by=user,
