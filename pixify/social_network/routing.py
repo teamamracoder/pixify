07321c26de97bc6e1251d13ea048a93779ff6853
subham <modaksuham866@gmail.com>
2025-02-13 18:51:57 +0530

    init.py,url

--- pixify/social_network/routing.py
+++ pixify/social_network/routing.py
@@ -3,5 +3,4 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<chat_id>\d+)/$', consumers.ChatConsumer.as_asgi()),  # Capture chat_id dynamically 
-    re_path(r'ws/call/(?P<chat_id>\w+)/$', consumers.CallConsumer.as_asgi()),   
 ]
