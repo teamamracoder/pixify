30ac3ff3df2b11fb9941e00866141cc74970b049
partha <partha.sutradharmy@gmail.com>
2025-02-14 00:53:34 +0530

    Shorts Redirect from home Carousel

--- pixify/social_network/urls.py
+++ pixify/social_network/urls.py
@@ -180,8 +180,7 @@ urlpatterns = [
     path('short/<int:post_id>/comment/create/', views.ShortCommentCreateView.as_view(),name='short_comment_create'),
     path('short/comment/<int:comment_id>/delete/', views.ShortCommentDeleteView.as_view(),name='short_comment_delete'),
     path('short/comment/<int:comment_id>/reply/', views.ShortCommentReplyView.as_view(),name='short_comment_reply'),
-    path('short/comment/<int:comment_id>/reaction/', views.ShortCommentReactionView.as_view(),name='short_comment_reaction'),    
-    path('short/<int:post_id>/', views.ShortVisitedView.as_view(), name='short-detail'),
+    path('short/comment/<int:comment_id>/reaction/', views.ShortCommentReactionView.as_view(),name='short_comment_reaction'),
 ]
 
 
