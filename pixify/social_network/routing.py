67a053aafb873cd1fc3862a4cfd4e73c0e720d02
partha <partha.sutradharmy@gmail.com>
2025-03-15 18:11:26 +0530

    Add CallConsumer for managing WebSocket calls and user connections

--- pixify/social_network/routing.py
+++ pixify/social_network/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
-from . import consumers
+from . import consumers, calll_consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/call/(?P<chat_id>\w+)/$', consumers.CallConsumer.as_asgi()),    
+    re_path(r'ws/call/(?P<chat_id>\w+)/$', calll_consumers.CallConsumer.as_asgi()),    
 ]
