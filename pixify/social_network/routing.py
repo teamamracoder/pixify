bea76835e29be08e32f551a13b68122e5a9a4b5e
Subham <modaksubham866@gmail.com>
2025-03-20 10:48:01 +0530

    "make callconsumer"

--- pixify/social_network/routing.py
+++ pixify/social_network/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
 from . import consumers
+from .callconsumer import CallConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/call/(?P<chat_id>\w+)/$', consumers.CallConsumer.as_asgi()),    
+    re_path(r'ws/call/(?P<chat_id>\w+)/$',CallConsumer.as_asgi()),    
 ]
